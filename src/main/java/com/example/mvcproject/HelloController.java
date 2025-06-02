63aa49b1e89c6134e2e917ca013d5317a77f5427
kakusiA <wtecho331@gmail.com>
2025-06-02 16:48:08 +0900

    no message

--- src/main/java/com/example/mvcproject/HelloController.java
+++ src/main/java/com/example/mvcproject/HelloController.java
@@ -1,11 +1,15 @@
 package com.example.mvcproject;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-
+    @RequestMapping("/")
+    public String index() {
+        return "redirect:/login";
+    }
     @RequestMapping("/hello")
     public String home(){
         return"hello";
